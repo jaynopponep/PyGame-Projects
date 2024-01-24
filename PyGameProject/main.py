6cd5ce66356b6b386e14ed4784ea7ba3c7684e27
jaynopponep <noppone.p@gmail.com>
2024-01-24 18:09:46 -0500

    Added shooting rocks ability

--- PyGameProject/main.py
+++ PyGameProject/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # initialize pygame
 pygame.init()
@@ -10,14 +11,30 @@ pygame.display.set_icon(icon)
 
 # player
 playerImg = pygame.image.load('shovel.png')
-playerX = 370
-playerY = 480
+playerX = 350
+playerY = 680
 playerX_change = 0
 
+
+# enemy
 enemyImg = pygame.image.load('catpoo.png')
-enemyX = 370
-enemyY = 50
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
 enemyX_change = 0
+enemyY_change = 20
+
+
+# rocks
+rocksImg = pygame.image.load('litterrocks.png')
+rocksX = 0
+rocksY = 480
+rocksX_change = 0
+rocksY_change = 10
+rock_state = "ready"
+
+
+# background
+background = pygame.image.load('living_room.jpg')
 
 
 def player(x, y):
@@ -28,13 +45,20 @@ def enemy(x, y):
     screen.blit(enemyImg, (x, y))
 
 
+def shoot_rock(x, y):
+    global rock_state
+    rock_state = "shoot"
+    screen.blit(rocksImg, (x+16, y+10))
+
+
 # render screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 800))
 # problem, screen renders but goes away almost instantly. fix:
 running = True
 while running:
     # screen RGB fill
     screen.fill((255, 253, 208))
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False              # running is set to False if we detect pygame.QUIT after staying on True
@@ -43,16 +67,28 @@ while running:
                 playerX_change = -4
             if event.key == pygame.K_RIGHT:
                 playerX_change = 4
+            if event.key == pygame.K_SPACE:
+                shoot_rock(playerX, rocksY)
+
         if event.type == pygame.KEYUP:   # KEYUP means the keystroke is released
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
+    # player movement
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
-    elif playerX >= 736:
-        playerX = 736
+    elif playerX >= 800:
+        playerX = 800
+    # enemy movement
+    enemyX += enemyX_change
+    enemyY += 0.5
     player(playerX, playerY)
     enemy(enemyX, enemyY)
+    # rock movement
+    if rock_state is "shoot":
+        shoot_rock(playerX, rocksY)
+        rocksY -= rocksY_change
     pygame.display.update()
+
 
 
